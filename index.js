65cb66d358a7382e7a99bbb08295466a0df20ed3
CarlosValdiviaEspino <carlord5272009@gmail.com>
2021-12-14 19:32:28 -0500

    agrandar y resaltar 2

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function obtenerTamaño() {
-    var fontSize = 12;
+    
     tamaño = document.getElementById('tamañotexto').value;
     fontSize =  getComputedStyle(document.documentElement).getPropertyValue('--font-size')
     fontSize  = tamaño
-    document.documentElement.style.setProperty('--font-size', `${fontSize/50}`)
+    // al dividir el fontsize entre un numero menor, mas se agranda.
+    document.documentElement.style.setProperty('--font-size', `${fontSize/40}`)
 
 }
 
@@ -13,7 +14,6 @@ function obtenerColor() {
 
     textos = document.getElementsByTagName("p");
     var i = 0;
-
     for (i = 0; i < textos.length; i++) {
         textos[i].style.backgroundColor = color;
       }
